a135c050d0ea06e6515229b75bfe758c44f56d1c
Soyon <soyonchoi29@gmail.com>
2022-04-28 07:12:45 -0400

    Beginning crop functionality

---
+++ src/ImageChange.java
@@ -0,0 +1,25 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.lang.Math;
+import java.awt.image.BufferedImage;
+
+public class ImageChange {
+    
+    public static BufferedImage cropImage (File input, int x, int y, int width, int height){
+        try{
+            BufferedImage originalImage = ImageIO.read(input);
+            BufferedImage croppedImage = originalImage.getSubimage(x, y, width, height);
+            return croppedImage;
+        }catch(IOException error){return null;}
+    }
+
+    public static void ImageStitch(){
+
+    }
+}
