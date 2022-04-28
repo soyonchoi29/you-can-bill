69dab28736912b30f17e7f78e8d9398827ae99d2
ktdai <kevin.daiwang@gmail.com>
2022-04-27 22:45:39 -0400

    Scale and rotate input image

--- src/ImagePopup.java
+++ src/ImagePopup.java
@@ -1,5 +1,8 @@
 
 import java.awt.image.BufferedImage;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -7,12 +10,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.lang.Math;
 
 /*
  *  Code example to add an image into JPanel
  */
 public class ImagePopup {
-
     //public static void main(String[] args) throws IOException {
     //}
 
@@ -21,7 +24,16 @@ public class ImagePopup {
             JFrame frame = new JFrame("Selected receipt"); // change later???
             JPanel panel = new JPanel();
             BufferedImage myPicture = ImageIO.read(input);
-            JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+            Image resized = myPicture.getScaledInstance(500, 500, 2); // Resizes image to fit entire upload into JPanel
+
+            AffineTransform transform = AffineTransform.getRotateInstance(Math.toRadians (90), 250, 250); // ROTATE BY 90 (CAN CHANGE)
+            AffineTransformOp operation = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+            BufferedImage rotated = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB); // Creates new BufferedImage, then fills it in with rotated/scaled version
+            Graphics2D redraw = rotated.createGraphics();
+            redraw.drawImage(resized, 0, 0, null);
+            redraw.dispose();
+
+            JLabel picLabel = new JLabel(new ImageIcon(operation.filter(rotated, null)));
             panel.add(picLabel);
             frame.add(panel);
             frame.setSize(500, 500); // perhaps cropping is drawing new jpanels on top or something
@@ -31,3 +43,7 @@ public class ImagePopup {
 }
 
 //Based on https://www.javaexercise.com/java/add-an-image-to-a-jpanel - citation needed
+//https://docs.oracle.com/javase/7/docs/api/java/awt/Image.html#getScaledInstance(int,%20int,%20int)
+// https://mkyong.com/java/how-to-resize-an-image-in-java/
+//https://stackoverflow.com/questions/13605248/java-converting-image-to-bufferedimage
+//https://stackoverflow.com/questions/8639567/java-rotating-images
