e4a0410f452bef5fc0ab2e85a57eaaf96d50f579
Soyon <soyonchoi29@gmail.com>
2022-04-28 21:03:37 -0400

    Comment out unnecessary fields for now

--- src/Item.java
+++ src/Item.java
@@ -1,13 +1,23 @@
+import java.awt.image.BufferedImage;
 
 public class Item {
-    private final String name;
-    private int quantity;
+    //private final String name;
+    //private int quantity;
     private float price;
     private Person payer;
+    private BufferedImage image;
 
-    public Item(String name, int quantity, float price) {
-        this.name = name;
-        this.quantity = quantity;
-        this.price = price;
+    //public Item(String name, int quantity, float price) {
+    //    this.name = name;
+    //    this.quantity = quantity;
+    //    this.price = price;
+    //}
+
+    public Item(BufferedImage image){
+        this.image = image;
+    }
+
+    public BufferedImage getImage(){
+        return this.image;
     }
 }
