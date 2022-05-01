bcd1f7edbe1bb441ddc87914a7c358e0d08f831e
Ris Paulino <risantpaul@gmail.com>
2022-05-01 14:08:49 -0400

    Cropping functionality- Ris

--- src/ImagePopup.java
+++ src/ImagePopup.java
@@ -1,4 +1,154 @@
 
+// import java.awt.image.BufferedImage;
+// import java.awt.*;
+// import java.awt.geom.AffineTransform;
+// import java.awt.image.AffineTransformOp;
+// import java.io.File;
+// import java.io.IOException;
+// import javax.imageio.ImageIO;
+// import javax.swing.ImageIcon;
+// import javax.swing.JFrame;
+// import javax.swing.JLabel;
+// import javax.swing.JPanel;
+// import java.lang.Math;
+
+// import javax.swing.*;
+// import javax.swing.filechooser.FileNameExtensionFilter;
+
+// import org.w3c.dom.events.MouseEvent;
+// import java.awt.event.MouseListener;
+// import java.awt.event.MouseMotionListener;
+
+// import java.awt.BorderLayout;
+// import java.awt.Color;
+// import java.awt.event.*;
+// import java.io.*;
+// import java.awt.image.*;
+// import java.awt.FlowLayout;
+
+// public class ImagePopup {
+//     //public static void main(String[] args) throws IOException {
+//     //}
+
+//     static int counter = 0;
+//     public static void drawNew(File input) {
+//         JFrame frame = new JFrame("Selected receipt"); // change later???
+//         JPanel panel = new JPanel();
+//         BufferedImage myPicture = null;
+//         try {
+//             myPicture = ImageIO.read(input);
+//         } catch (IOException error) {}
+
+//         JButton rotateB = new JButton("Rotate Image");
+//         rotateB.setBounds(0, 0, 50, 30); // Might need to move
+//         panel.add(rotateB);
+
+//         Image resized = myPicture.getScaledInstance(500, 500, 2); // Resizes image to fit entire upload into JPanel
+//         //AffineTransform transform = AffineTransform.getRotateInstance(Math.toRadians (90), 200, 250); // ROTATE BY 90 (CAN CHANGE) - DON'T NEED IF BUTTONS
+//         //AffineTransformOp operation = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR); // some sort of rotation option
+//         //BufferedImage rotated = new BufferedImage(400, 500, BufferedImage.TYPE_INT_ARGB); // Creates new BufferedImage, then fills it in with rotated/scaled version
+//         //Graphics2D redraw = rotated.createGraphics(); // Turns rotated into Graphics2D object and draws it
+//         //redraw.drawImage(resized, 0, 0, null);
+//         //redraw.dispose();
+
+//         //JLabel picLabel = new JLabel(new ImageIcon(operation.filter(rotated, null)));
+//         JLabel picLabel = new JLabel(new ImageIcon(resized));
+//         panel.add(picLabel);
+//         frame.add(panel);
+//         frame.setSize(500, 600); // perhaps cropping is drawing new jpanels on top or something
+//         frame.setVisible(true); // will need to recenter receipt better most likely
+//         // Looking for while button not being pressed
+//         //Maybe some sort of an instructional popup
+//         rotateB.addActionListener(new ActionListener(){
+
+//             @Override
+//             public void actionPerformed(ActionEvent e){
+//                 counter++;
+//                 //System.out.println("reeeeeee");
+//                 //Get the components in the panel
+//                 Component[] componentList = panel.getComponents();
+
+//                 //Loop through the components
+//                 for(Component c : componentList){ // https://stackoverflow.com/questions/7117332/dynamically-remove-component-from-jpanel
+
+//                     //Find the components you want to remove
+//                     if(c instanceof JLabel){
+
+//                         //Remove it
+//                         panel.remove(c);
+//                     }
+//                 }
+//                 panel.validate();
+//                 panel.repaint(); // only rotates the first time, then doesn't rotate again - maybe solve with a call to external method???
+//                 AffineTransform transform = AffineTransform.getRotateInstance(Math.toRadians (90 * counter), 250, 250); // ROTATE BY 90 (CAN CHANGE)
+//                 AffineTransformOp operation = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+//                 BufferedImage rotated = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB); // Creates new BufferedImage, then fills it in with rotated/scaled version
+//                 Graphics2D redraw = rotated.createGraphics(); // Turns rotated into Graphics2D object and draws it
+//                 redraw.drawImage(resized, 0, 0, null);
+//                 redraw.dispose();
+
+//                 JLabel picLabel = new JLabel(new ImageIcon(operation.filter(rotated, null)));
+//                 panel.add(picLabel);
+
+//                 frame.setVisible(true);
+//             }
+
+//         });
+//     }
+
+//     public static void redraw(JLabel newDraw) {
+//         //drawNew.add(picLabel);
+//         //frame.setVisible(true);
+//     }
+
+// }
+
+// class JImageCropComponent extends JComponent implements MouseListener, MouseMotionListener{
+//     private BufferedImage image;
+//     private BufferedImage croppedImage;
+//     private int x1, y1, x2, y2;
+//     private Color cropToolColor = new Color(201, 221, 240);
+//     private boolean cropping = false;
+
+//     public JImageCropComponent(BufferedImage image){
+//         this.image = image;
+//         this.addMouseListener(this);
+//         this.addMouseMotionListener(this);
+//     }
+
+//     public void setImage(BufferedImage image){
+//         this.image = image;
+//     }
+
+//     @Override
+//     public void paintComponent(Graphics g){
+//         if (cropping){
+//             //Mark the area being cropped
+//             g.setColor(cropToolColor);
+//             g.drawRect(Math.min(x1,x2), Math.min(y1,y2), Math.max(x1,x2)-Math.min(x1,x2), Math.max(y1,y2)-Math.min(y1,y2));
+//         }
+//     }
+
+//     @Override
+//     public void mousePressed(MouseEvent e){
+//         this.x1 = e.getClientX();
+//         this.x2 = e.getClientY();
+//     }
+
+//     @Override
+//     public void mouseReleased(MouseEvent e){
+//         this.cropping = false;
+//         this.croppedImage = this.image.getSubimage(Math.min(x1,x2), Math.min(y1,y2), Math.max(x1,x2)-Math.min(x1,x2), Math.max(y1,y2)-Math.min(y1,y2));
+//     }
+
+//     @Override
+//     public void mouseDragged(MouseEvent e){
+//         this.cropping = true;
+//         this.x2 = e.getClientX();
+//         this.y2 = e.getClientY();
+//     }
+// }
+
 import java.awt.image.BufferedImage;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
@@ -11,29 +161,34 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.lang.Math;
-
 import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
-import org.w3c.dom.events.MouseEvent;
+import java.awt.event.*;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.event.*;
-import java.io.*;
-import java.awt.image.*;
-import java.awt.FlowLayout;
-
-public class ImagePopup {
-    //public static void main(String[] args) throws IOException {
-    //}
-
+public class ImagePopup extends JFrame implements MouseListener, MouseMotionListener {
+    boolean doneCropping = false;
+    private int x1, y1, x2, y2;
     static int counter = 0;
+
     public static void drawNew(File input) {
+        new ImagePopup().crop(input);
+    }
+
+    public void crop(File input) {
         JFrame frame = new JFrame("Selected receipt"); // change later???
         JPanel panel = new JPanel();
+        panel.setSize(500,600);
+        panel.setVisible(true);
+        panel.addMouseListener(this);
+        panel.addMouseMotionListener (this);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         BufferedImage myPicture = null;
         try {
             myPicture = ImageIO.read(input);
@@ -52,13 +207,14 @@ public class ImagePopup {
         //redraw.dispose();
 
         //JLabel picLabel = new JLabel(new ImageIcon(operation.filter(rotated, null)));
-        JLabel picLabel = new JLabel(new ImageIcon(resized));
-        panel.add(picLabel);
+        // JLabel picLabel = new JLabel(new ImageIcon(resized));
+        // panel.add(picLabel);
         frame.add(panel);
         frame.setSize(500, 600); // perhaps cropping is drawing new jpanels on top or something
         frame.setVisible(true); // will need to recenter receipt better most likely
         // Looking for while button not being pressed
         //Maybe some sort of an instructional popup
+
         rotateB.addActionListener(new ActionListener(){
 
             @Override
@@ -89,64 +245,95 @@ public class ImagePopup {
 
                 JLabel picLabel = new JLabel(new ImageIcon(operation.filter(rotated, null)));
                 panel.add(picLabel);
-
                 frame.setVisible(true);
             }
 
         });
+
+        JButton mainMenu = new JButton("Main Menu");
+        mainMenu.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                frame.setVisible(false);
+            }
+        });
+        panel.add(mainMenu);
+
     }
 
-    public static void redraw(JLabel newDraw) {
-        //drawNew.add(picLabel);
-        //frame.setVisible(true);
+    public void croppedImage() throws Exception {
+        int width = x2 - x1;
+        int height = y2 - y1;
+
+        if(width < 0) {
+            width = -1 * width;
+        }
+        if(height < 0) {
+            height = -1 * height;
+        }
+    
+        Robot robot = new Robot();
+        BufferedImage image = robot.createScreenCapture(new Rectangle(x1, y1, width, height));
+        File croppedImage = new File ("croppedImage.jpg");
+        ImageIO.write(image, "jpg", croppedImage);
     }
 
-}
+    public void paintComponent(Graphics g) {
+        super.paint(g);
+        int width = x2 - x1;
+        int height = y2 - y1;
 
-class JImageCropComponent extends JComponent implements MouseListener, MouseMotionListener{
-    private BufferedImage image;
-    private BufferedImage croppedImage;
-    private int x1, y1, x2, y2;
-    private Color cropToolColor = new Color(201, 221, 240);
-    private boolean cropping = false;
+        if(width < 0) {
+            width = -1 * width;
+        }
 
-    public JImageCropComponent(BufferedImage image){
-        this.image = image;
-        this.addMouseListener(this);
-        this.addMouseMotionListener(this);
-    }
+        if(height < 0) {
+            height = -1 * height;
+        }
 
-    public void setImage(BufferedImage image){
-        this.image = image;
+        g.drawRect(x1, y1, width, height);
     }
 
     @Override
-    public void paintComponent(Graphics g){
-        if (cropping){
-            //Mark the area being cropped
-            g.setColor(cropToolColor);
-            g.drawRect(Math.min(x1,x2), Math.min(y1,y2), Math.max(x1,x2)-Math.min(x1,x2), Math.max(y1,y2)-Math.min(y1,y2));
+    public void mousePressed(MouseEvent e) {
+        repaint();
+        x1 = e.getX();
+        x2 = e.getY();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        repaint();
+        if(doneCropping == true) {
+            x2 = e.getX();
+            y2 = e.getY();
+            try {
+                croppedImage();
+            }
+            catch(Exception a){
+                a.printStackTrace();
+            }
         }
     }
 
     @Override
-    public void mousePressed(MouseEvent e){
-        this.x1 = e.getClientX();
-        this.x2 = e.getClientY();
+    public void mouseDragged (MouseEvent e) {
+        repaint();
+        doneCropping = true;
+        x2 = e.getX();
+        y2 = e.getY();
     }
 
     @Override
-    public void mouseReleased(MouseEvent e){
-        this.cropping = false;
-        this.croppedImage = this.image.getSubimage(Math.min(x1,x2), Math.min(y1,y2), Math.max(x1,x2)-Math.min(x1,x2), Math.max(y1,y2)-Math.min(y1,y2));
-    }
+    public void mouseMoved(MouseEvent e) {}
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {}
 
     @Override
-    public void mouseDragged(MouseEvent e){
-        this.cropping = true;
-        this.x2 = e.getClientX();
-        this.y2 = e.getClientY();
-    }
+    public void mouseEntered(MouseEvent e) {}
+ 
+    @Override
+    public void mouseExited(MouseEvent e) {}
 }
 
 //Based on https://www.javaexercise.com/java/add-an-image-to-a-jpanel
