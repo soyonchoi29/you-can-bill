0673e7a1d908426f2538f17a6ac53b638a04e373
ktdai <kevin.daiwang@gmail.com>
2022-05-15 14:26:03 -0400

    Remove unneeded files

--- src/test.java
+++
@@ -1,8 +0,0 @@
-
-class test {
-    public static void main(String[] args){
-        System.out.println("Test");
-        System.out.println("Hi");
-        System.out.println("HI friends");
-    }
-}
