f32e0ae9a5665e10c6fa20c2c57d628003a312b6
ktdai <kevin.daiwang@gmail.com>
2022-05-15 14:41:51 -0400

    Remove more unneeded comments

--- src/Item.java
+++ src/Item.java
@@ -1,17 +1,9 @@
 import java.awt.image.BufferedImage;
 
 public class Item {
-    //private final String name;
-    //private int quantity;
     private float price;
     private Customer payer;
     private BufferedImage image;
-
-    //public Item(String name, int quantity, float price) {
-    //    this.name = name;
-    //    this.quantity = quantity;
-    //    this.price = price;
-    //}
 
     public Item(BufferedImage image){
         this.image = image;
